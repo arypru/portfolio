c842f2b52fc3b17043303928ba05b7aa7ba6c4f3
arypru <arapruy@gmail.com>
2022-08-04 18:48:53 -0300

    :first_quarter_moon: feat:componente color mode

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,10 @@
 <template>
   <div>
+    <VButtonColorMode/>
+    <!--
     <p class="underline decoration-wavy font-amaranth">Prueba Amaranth</p>
     <p class="underline decoration-wavy font-darkergrotesque">Prueba Darker Grotesque</p>
-
-    <div>
-      <h1>Color mode: {{ $colorMode.value }}</h1>
-      <select v-model="$colorMode.preference">
-        <option value="system">System</option>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
-        <option value="sepia">Sepia</option>
-      </select>
-    </div>
-
+-->
   </div>
 </template>
 
@@ -21,18 +13,3 @@ export default {
   name: 'IndexPage'
 }
 </script>
-
-<style>
-body {
-  background-color: #fff;
-  color: rgba(0,0,0,0.8);
-}
-.dark-mode body {
-  background-color: #091a28;
-  color: #ebf4f1;
-}
-.sepia-mode body {
-  background-color: #f1e7d0;
-  color: #433422;
-}
-</style>
