9801afeb615e67f8509f2f6858e3175d3944f747
arypru <arapruy@gmail.com>
2022-08-05 23:27:27 -0300

    :framed_picture: feat: add images icons
    Creacion carpeta images con emoticonos

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,13 @@
     <VButtonColorMode/>
     <p class="underline decoration-wavy font-darkergrotesque">Prueba Darker Grotesque</p>
     <VTitle texto-titulo="Titulo"/>
+
+    <div class="flex flex-row items-center justify-center">
+      <h2 class="text-5xl font-amaranth"> Hola pepe </h2>
+      <img class="w-14 h-14" src="/images/icons/medalla.png">
+    </div>
+
+
   </div>
 </template>
 <script>
