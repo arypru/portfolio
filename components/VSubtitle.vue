28f1891bcc5dd7f2b7bc6788591e8d7dc7622e90
arypru <arapruy@gmail.com>
2022-08-05 23:45:32 -0300

    :pencil2: feat: add component subtitle
    Creacion del componente Sub Titulo.
    Recibe de props string "textoSubTitulo" y 'emoji'

---
+++ components/VSubtitle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-row items-center justify-center">
+    <h2 class="text-3xl md:text-5xl font-amaranth mr-2"> {{textoSubTitulo}} </h2>
+    <img class="w-10 h-10 md:w-14 md:h-14" :src="emoji">
+  </div>
+</template>
+
+<script>
+export default {
+
+  props: {
+
+    textoSubTitulo: {
+      type: String,
+      required: true,
+    },
+
+    emoji:{
+      type: String,
+      required: true
+    }
+  }
+
+}
+</script>
