09820e6ec9aa7f9d678bcefad0811d270af2dc55
arypru <arapruy@gmail.com>
2022-08-06 22:34:32 -0300

    :link: feat : add component link
    Creación del componente link con las siguientes props requeridas y del tipo string
    -textoLink
    -Texto View Box
    -Texto SVG

---
+++ components/VLink.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="flex flex-row items-center justify-center">
+    <p class="tracking-wide leading-relaxed font-darkergrotesque font-medium text-base md:text-2xl"> {{textoLink}} </p>
+    <svg class="h-4 w-4 md:h-6 md:w-6" xmlns="http://www.w3.org/2000/svg" :viewBox="textViewBox" xml:space="preserve">
+      <path :d="textSvg"/>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+
+  props: {
+    textViewBox: {
+      type: String,
+      required: true,
+    },
+    textSvg: {
+      type: String,
+      required: true,
+    },
+    textoLink: {
+      type: String,
+      required: true,
+    },
+  }
+}
+</script>
+
+<style scoped>
+.dark-mode svg {
+  fill:#FFFFFF;
+}
+.light-mode p {
+  color:#FF0E47
+}
+.light-mode svg {
+  fill:#FF0E47
+}
+</style>
