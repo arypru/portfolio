add13d55ebaed0bd95827ce72b7d17caf523cdec
arypru <arapruy@gmail.com>
2022-08-07 00:27:00 -0300

    :framed_picture: feat : add component icon
    Creacion del componente icon del cual espera como props un string
    src-imagen

---
+++ components/VIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <img alt="" class="justify-center-center grayscale hover:grayscale-0 h-14 w-14" :src="scrImagen">
+</template>
+
+<script>
+export default {
+  props:{
+    srcImagen: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
