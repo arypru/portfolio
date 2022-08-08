9ea95d35b45443f7d25ec07681efe635b3b0f2f7
arypru <arapruy@gmail.com>
2022-08-08 19:41:30 -0300

    :heavy_plus_sign: feat: add section sobre mi
    Creacion de la seccion "sobre mi"

---
+++ components/TheSectionAboutMe.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <VTitle texto-titulo="Araceli Pruyas de los Angeles I."/>
+    <div class="my-3 md:flex lg:flex-row md:justify-center">
+        <VDescription v-for="descrip in descripciones" :key="descrip.textoDescripcion" class="md:mx-4 md:my-3" :texto-descripcion="descrip.textoDescripcion" :emoji="descrip.icon"/>
+    </div>
+    <VDescription texto-descripcion="Me encanta ser Frontend Developer porque me permite combinar diseño + programacion + creatividad" :emoji="'/images/icons/star.png'"/>
+  </section>
+</template>
+
+<script>
+import VTitle from "./VTitle";
+import VDescription from "./VDescription";
+
+export default {
+  components: {VTitle,VDescription,VIcon},
+
+  data() {
+    return {
+      descripciones: [
+        {textoDescripcion:'Frontend Developer',icon:'/images/icons/computer.png'},
+        {textoDescripcion:'Vue.js', icon:'/images/vuejs.svg'},
+        {textoDescripcion:'Argentina', icon:'/images/icons/argentina.png'},
+      ],
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,15 @@
 <template>
   <div>
-    <VTitle texto-titulo="Titulo"/>
-    <VSubtitle texto-sub-titulo="Sub Titulo" emoji="/images/icons/argentina.png"/>
-    <VDescription texto-descripcion="consectetur adipisicing elit. Animi atque commodi doloremque et odit, sed suscipit! Impedit quas ratione voluptatem?" emoji="/images/icons/star.png"/>
-    <VButton texto-button="Me interesa ver mas de tus proyectos "/>
+    <TheSectionAboutMe/>
     <VDivider/>
-    <VIcon src-imagen="/images/vuejs.svg"/>
-    <VCard nombre-proyecto="Sistema de Expedientes" herramientas="Vue.js - Vuetify - Axios" descripcion-proyecto=" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, nam? Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, impedit. Lorem ipsum dolor sit amet."/>
-  </div>
+ </div>
 </template>
 
 <script>
-import VTitle from "../components/VTitle";
-import VSubtitle from "../components/VSubtitle";
-import VDescription from "../components/VDescription";
-import VButton from "../components/VButton";
-import VIcon from "../components/VIcon";
+import TheSectionAboutMe from "../components/TheSectionAboutMe";
 import VDivider from "../components/VDivider";
-import VCard from "../components/VCard";
 
 export default {
-  components: {VIcon, VButton, VDescription, VSubtitle, VTitle,VDivider, VCard}
+  components: {VDivider, TheSectionAboutMe}
 }
 </script>
