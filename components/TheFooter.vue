7b94a4638284b026e7472fba6d4b74b01f73c753
arypru <arapruy@gmail.com>
2022-08-08 16:40:43 -0300

    :heavy_plus_sign: feat: add component footer
    Creacion del componente Footer

---
+++ components/TheFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="p-4 bg-white border-t mt-8">
+    <p class="tracking-wide leading-relaxed font-darkergrotesque font-medium text-lg text-center ">
+      Hecho con Nuxt.js - Tailwind CSS ♥
+    </p>
+    <p class="tracking-wide leading-relaxed font-darkergrotesque font-medium text-lg text-center ">
+      2022
+    </p>
+  </footer>
+</template>
+
+<script>
+
+</script>
