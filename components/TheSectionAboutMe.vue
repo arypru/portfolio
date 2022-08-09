baa04e26065d2bfab4eebeba3b7bd996125b11f7
arypru <arapruy@gmail.com>
2022-08-08 21:26:14 -0300

    :heavy_plus_sign: feat: add json DescripcionSobreMi y RedesSociales
    Aislamiento de los datos cargados en TheSectionAboutMe a json para asi evitar tocar el componente en caso de cambio de textos.

--- components/TheSectionAboutMe.vue
+++ components/TheSectionAboutMe.vue
@@ -5,24 +5,32 @@
         <VDescription v-for="descrip in descripciones" :key="descrip.textoDescripcion" class="md:mx-4 md:my-3" :texto-descripcion="descrip.textoDescripcion" :emoji="descrip.icon"/>
     </div>
     <VDescription texto-descripcion="Me encanta ser Frontend Developer porque me permite combinar diseño + programacion + creatividad" :emoji="'/images/icons/star.png'"/>
+    <div class="my-3 md:flex md:flex-row md:justify-center">
+      <VLink class="mx-5" v-for="red in redesSociales" :key="red.textoLink" :texto-link="red.textoLink" :link="red.link" :text-svg="red.textSvg"/>
+    </div>
   </section>
 </template>
 
 <script>
 import VTitle from "./VTitle";
 import VDescription from "./VDescription";
+import VLink from "./VLink";
+
+import descripcionesJson from "/static/json/DescripcionSobreMi.json";
+import redesSocialesJson from "/static/json/RedesSociales.json"
 
 export default {
-  components: {VTitle,VDescription,VIcon},
+  components: {VTitle,VDescription,VLink},
 
-  data() {
-    return {
-      descripciones: [
-        {textoDescripcion:'Frontend Developer',icon:'/images/icons/computer.png'},
-        {textoDescripcion:'Vue.js', icon:'/images/vuejs.svg'},
-        {textoDescripcion:'Argentina', icon:'/images/icons/argentina.png'},
-      ],
+  computed: {
+    descripciones() {
+      return descripcionesJson.descripciones
+    },
+
+    redesSociales() {
+      return redesSocialesJson.redesSociales
     }
   }
+
 }
 </script>
