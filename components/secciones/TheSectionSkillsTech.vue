ea9a512c43862ae0f3049d399314f756706c51df
arypru <arapruy@gmail.com>
2022-08-15 18:08:19 -0300

    :heavy_plus_sign: feat: add component vskill

--- components/secciones/TheSectionSkillsTech.vue
+++ components/secciones/TheSectionSkillsTech.vue
@@ -1,51 +1,12 @@
 <template>
   <section>
     <VSubtitle emoji="/images/icons/medalla.png" texto-sub-titulo="Habilidades"/>
-
-    <h3 class="text-2xl font-semibold "> Frontend Developer</h3>
-    <div class="flex flex-row py-8">
-        <span v-for="skill in skillsFrontend"
-              class="px-4 py-2 my-1.5 font-darkergrotesque bg-transparent border border-fucsia text-xl rounded">
-        {{ skill }}
-      </span>
+    <div class="space-y-12">
+      <VSkill :skills="skillsFrontend" :nombre-skill="'Frontend Developer'"/>
+      <VSkill :skills="skillsUxUi" :nombre-skill="'Design UI/UX'"/>
+      <VSkill :skills="skillsBack" :nombre-skill="'Backend Developer'"/>
+      <VSkill :skills="skillsAgile" :nombre-skill="'Agile Mindset'"/>
     </div>
-
-    <h3 class="text-2xl font-semibold"> Frontend Developer </h3>
-    <div class="space-x-4 flex flex-row flex-wrap">
-      <p v-for="skill in skillsUxUi"
-         class="px-4 py-2 my-1.5 font-darkergrotesque bg-transparent border border-fucsia text-xl rounded">
-        {{ skill }}
-      </p>
-    </div>
-
-    <h3 class="text-2xl font-semibold"> Design UI/UX</h3>
-    <div class="space-x-4 flex flex-row flex-wrap">
-      <p v-for="skill in skillsUxUi"
-         class="px-4 py-2 my-1.5 font-darkergrotesque bg-transparent border border-fucsia text-xl rounded">
-        {{ skill }}
-      </p>
-    </div>
-
-    <h3 class="text-2xl font-semibold"> Backend Developer</h3>
-    <div class="space-x-4 flex flex-row flex-wrap">
-      <p v-for="skill in skillsBack"
-         class="px-4 py-2 my-1.5 font-darkergrotesque bg-transparent border border-fucsia text-xl rounded">
-        {{ skill }}
-      </p>
-    </div>
-
-    <h3 class="text-2xl font-semibold"> Agile mindset </h3>
-    <div class="space-x-4 flex flex-row flex-wrap">
-      <p v-for="skill in skillsAgile"
-         class="px-4 py-2 my-1.5 font-darkergrotesque bg-transparent border border-fucsia text-xl rounded">
-        {{ skill }}
-      </p>
-    </div>
-    <!--<div class="flex flex-row flex-wrap justify-evenly space-x-6 space-y-4 md:my-4 lg:my-6 ">
-      <VIcon v-for="skill in skills" :key="skill.textoSkill" :src-imagen="skill.srcImagen" :texto-skills="skill.textoSkill"/>
-    </div>-->
-
-
   </section>
 </template>
 
@@ -53,14 +14,15 @@
 import VSubtitle from "../VSubtitle";
 import SkillsJson from "/static/json/Skills.json";
 import VIcon from "../VIcon";
+import VSkill from "../VSkill";
 
 export default {
-  components: {VIcon, VSubtitle},
+  components: {VIcon, VSubtitle,VSkill},
 
   data() {
     return {
       skillsFrontend: ["Javascript", "CSS", "Html", "Vue.js",
-        "Vuetify", "Tailwind CSS", "Nuxt.js"],
+        "Nuxt.js", "Tailwind CSS", "Vuetify"],
 
       skillsUxUi: ["Prototipado", "Figma", "Adobe Illustrator", "Adobe Photoshop",
         "Entrevistas"],
